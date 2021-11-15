d900d5c44771619b8987f639b9d00fbff83f05ce
talentweb3 <92187491+talentweb3@users.noreply.github.com>
2021-11-15 22:33:03 +0300

    change

--- utils/qrCode.js
+++ utils/qrCode.js
@@ -1,6 +1,6 @@
 import QRCode from 'qrcode';
 
 export const genQRcode = async (url) => {
-    const QRcode = await QRCode.toDataURL(`http://localhost:8000/${url}`);
+    const QRcode = await QRCode.toDataURL(`https://pcrtest-centers.herokuapp.com${url}`);
     return QRcode;
 }
