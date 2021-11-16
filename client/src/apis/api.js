76694c72712cad30bf9bb46505c9f6eeccbafcf5
talentweb3 <92187491+talentweb3@users.noreply.github.com>
2021-11-16 04:47:10 +0300

    change

--- client/src/apis/api.js
+++ client/src/apis/api.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-var Url = `https://pcrtest-centers.herokuapp.com/`;
+// var Url = `https://pcrtest-centers.herokuapp.com/`;
 // if (process.env.NODE_ENV !== 'production')
 //     Url = `http://localhost:8000/`;
 // else
 //     Url = `https://pcrtest-centers.herokuapp.com/`;
 
-const Api = axios.create({ baseURL: `${Url}`});
-
+// const Api = axios.create({ baseURL: `${Url}`});
+const Api = axios.create();
 Api.interceptors.request.use((req) => {
     const user = JSON.parse(localStorage.getItem('profile'));
     if (user) {
