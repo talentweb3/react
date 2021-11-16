c01eede8d41426fd58a80f68f458910811b94096
talentweb3 <92187491+talentweb3@users.noreply.github.com>
2021-11-16 22:20:50 +0300

    change

--- controllers/applyController.js
+++ controllers/applyController.js
@@ -85,7 +85,8 @@ export const getApproved = async(req, res) => {
   const proposal1 = await Apply.findByIdAndUpdate(req.param('id'), {'resultAt':resulttime});
   const proposal = await Apply.findByIdAndUpdate(req.param('id'), {state: 1});
   const cloudinary_file_path = "https://http-dambr-net.mo.cloudinary.net/demo/image/upload/"; 
-  const tmp_rep = "app/public/upload";
+  const tmp_rep = "https://pcrtest-centers.herokuapp.com/app/public/upload";
+  // https://pcrtest-centers.herokuapp.com/
   switch (proposal.docType) {
     case 1:
       await generatePdfLab1(proposal, `${tmp_rep}/${proposal._id}.pdf`);
